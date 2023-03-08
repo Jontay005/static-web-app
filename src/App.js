26907e916dd12f08dfcd69a0acf7d4abe11be215
Jontay005 <johnwakaba@gmail.com>
2023-03-07 22:17:04 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Static Resume</h1>
+      <h2>React resume coming soon </h2>
+    </div>
+  );
+}
